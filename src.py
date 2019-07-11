9a76f9ac8fc2d959dab5fb3636fc570a021775be
ghazalb76 <ghazal.b93@gmail.com>
2019-07-11 22:34:56 +0430

    Add short time energy feature

--- src.py
+++ src.py
@@ -2,23 +2,27 @@
 import numpy, scipy, matplotlib.pyplot as plt, IPython.display as ipd
 import librosa, librosa.display
 import glob
+from scipy.io import wavfile
+import numpy as np
 
-
+print("AAAAAAAAAAAAAAAAAAAAAA")
 counter = 0
 koll = 0
 num_of_female_f = 0
 num_of_male_f = 0
 no_detection_f = 0
-path = "../data/male/*.wav"
+path = "data/male/*.wav"
+flag = True
 file_name_list = []
 for filename in glob.glob(path):
-    print(filename)
+    # print(filename)
     koll += 1
     x, sr = librosa.load(filename)
     X = scipy.fft(x)
     X_mag = numpy.absolute(X)        # spectral magnitude
     if(max(X_mag)>113.32542591236971 or max(X_mag)< 20.865958116020522):
         print('mardddddd')
+        flag = False
         num_of_male_f += 1
     else:
         fs, signal = wavfile.read(filename)
@@ -43,19 +47,28 @@ for filename in glob.glob(path):
 
         if max(STEs) <12.5 and max(STEs)>5:
             print("male")
+            flag = False
             num_of_male_f += 1
         elif max(STEs) >29:
             print("female")
+            flag = False
             num_of_female_f += 1
-        else:
-            if max(STEs) <20:
-                num_of_male_f += 1
-            elif max>20:
-                num_of_female_f += 1
             # print("BISEXUAL ")
             # no_detection_f += 0
-print(num_of_female_f)
-print(num_of_male_f)
-acc_f = num_of_female_f/len(female_name_list)
-print("ACCURACY:",acc_f)
+        else:
+            if max(STEs) <20:
+                print('male')
+                flag = False
+                num_of_male_f += 1
+            elif max(STEs)>20:
+                print('female')
+                flag = False
+                num_of_female_f += 1
+            # print("BISEXUAL ")
+            # no_detection_m += 0
+    # if(flag):
+    #     print('female')
+    #     num_of_female_f += 1
+print(num_of_male_f/koll)
+print(koll)
         
